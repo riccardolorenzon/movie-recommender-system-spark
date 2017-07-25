94f0bb937fcc68e2a9edf88b859419fc5b074b09
lobo-tech <riccardo.lorenzon@gmail.com>
2017-07-25 22:35:26 +0200

    Download datasets.

--- movie-recommender-system.py
+++ movie-recommender-system.py
@@ -1,2 +1,14 @@
 complete_dataset_url = 'http://files.grouplens.org/datasets/movielens/ml-latest.zip'
 small_dataset_url = 'http://files.grouplens.org/datasets/movielens/ml-latest-small.zip'
+
+import os
+
+datasets_path = os.path.join('..', 'datasets')
+
+complete_dataset_path = os.path.join(datasets_path, 'ml-latest.zip')
+small_dataset_path = os.path.join(datasets_path, 'ml-latest-small.zip')
+
+from urllib.request import urlretrieve
+
+small_f = urlretrieve (small_dataset_url, small_dataset_path)
+complete_f = urlretrieve (complete_dataset_url, complete_dataset_path)
