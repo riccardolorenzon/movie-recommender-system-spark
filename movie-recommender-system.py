ba53adff55939d5e93f3336282c822487524978d
lobo-tech <riccardo.lorenzon@gmail.com>
2017-08-01 21:39:14 +0200

    Set RDD for movies data.

--- movie-recommender-system.py
+++ movie-recommender-system.py
@@ -27,11 +27,23 @@ if not os.path.exists(complete_dataset_path):
 
 small_ratings_file = os.path.join(datasets_path, 'ml-latest-small', 'ratings.csv')
 
-# initiate an RDD with the ratings raw file
+# initiate a SparkConext
 from pyspark.context import SparkContext
 sc = SparkContext('local', 'movie-recommender-engine')
+
+# give small_ratings_file in input to the sc
 small_ratings_raw_data = sc.textFile(small_ratings_file)
 small_ratings_raw_data_header = small_ratings_raw_data.take(1)[0]
 
+# Parse the Raw data into a new RDD - Ratings.
 data = small_ratings_raw_data.filter(lambda line: line!=small_ratings_raw_data_header)\
     .map(lambda line: line.split(",")).map(lambda tokens: (tokens[0],tokens[1],tokens[2])).cache()
+
+small_movies_file = os.path.join(datasets_path, 'ml-latest-small', 'movies.csv')
+
+small_movies_raw_data = sc.textFile(small_movies_file)
+small_movies_raw_data_header = small_movies_raw_data.take(1)[0]
+
+# Parse the Raw data into a new RDD - Movies.
+small_movies_data = small_movies_raw_data.filter(lambda line: line!=small_movies_raw_data_header)\
+    .map(lambda line: line.split(",")).map(lambda tokens: (tokens[0],tokens[1])).cache()
